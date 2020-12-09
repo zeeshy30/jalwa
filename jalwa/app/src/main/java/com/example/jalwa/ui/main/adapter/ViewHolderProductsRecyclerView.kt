22cd5860ec578016d50018039ac70d9308edf550
zeeshyexper <74172459+zeeshyexper@users.noreply.github.com>
2020-12-10 00:42:02 +0500

    product design page complete

--- jalwa/app/src/main/java/com/example/jalwa/ui/main/adapter/ViewHolderProductsRecyclerView.kt
+++ jalwa/app/src/main/java/com/example/jalwa/ui/main/adapter/ViewHolderProductsRecyclerView.kt
@@ -16,21 +16,26 @@ class ViewHolderProductsRecyclerView(v: View) : RecyclerView.ViewHolder(v) {
     private val imageView: ImageView = v.findViewById(R.id.productImage)
     private val videoView: VideoView = v.findViewById(R.id.videoView)
     private val productPrice: TextView = v.findViewById(R.id.productPrice)
+    private val vendor: TextView = v.findViewById(R.id.vendor)
 
-    fun bind(context: Context, product: ProductsQuery.Product) {
-        title.text = product.title
-        productPrice.text = "Rs. ${product.price}"
-
-        Picasso.with(context).load(product.photoUrl).into(imageView)
-
+    private fun playVideo(context: Context, url: String) {
         val controller = MediaController(context)
         controller.setMediaPlayer(videoView)
         controller.isEnabled = true
         controller.show()
         controller.setMediaPlayer(videoView)
         videoView.setMediaController(controller)
-        videoView.setVideoPath(product.videoUrl)
+        videoView.setVideoPath(url)
         videoView.start()
+    }
+
+    fun bind(context: Context, product: ProductsQuery.Product) {
+        title.text = product.title
+        productPrice.text = "Rs. ${product.price}"
+        vendor.text = product.vendor
+
+        Picasso.with(context).load(product.photoUrl).into(imageView)
+        playVideo(context, product.videoUrl)
     }
 
 }
