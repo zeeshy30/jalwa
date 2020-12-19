3cc85ca65311a4ad74f82711d8015f1ce8fcd98e
zeeshyexper <74172459+zeeshyexper@users.noreply.github.com>
2020-12-20 03:46:49 +0500

    cleaning

--- jalwa/app/src/main/java/com/example/jalwa/ui/main/adapter/ViewHolderProductsRecyclerView.kt
+++ jalwa/app/src/main/java/com/example/jalwa/ui/main/adapter/ViewHolderProductsRecyclerView.kt
@@ -5,7 +5,6 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.jalwa.ProductsQuery
 import com.example.jalwa.databinding.ProductViewPageBinding
 import com.example.jalwa.ui.main.view.ProductDetailBottomSheet
-import com.squareup.picasso.Picasso
 
 class ViewHolderProductsRecyclerView(private val binding: ProductViewPageBinding)
     : RecyclerView.ViewHolder(binding.root) {
@@ -35,43 +34,3 @@ class ViewHolderProductsRecyclerView(private val binding: ProductViewPageBinding
         }
     }
 }
-//class ViewHolderProductsRecyclerView(v: View) : RecyclerView.ViewHolder(v) {
-//    private val title: TextView = v.findViewById(R.id.productTitle)
-//    private val imageView: ImageView = v.findViewById(R.id.productImage)
-//    private val videoView: VideoView = v.findViewById(R.id.videoView)
-//    private val productPrice: TextView = v.findViewById(R.id.productPrice)
-//    private val vendor: TextView = v.findViewById(R.id.vendor)
-//    private val buyNowButton: AppCompatButton = v.findViewById(R.id.buyNowProduct)
-//
-//    private fun playVideo(context: Context, url: String) {
-//        val controller = MediaController(context)
-//        controller.setMediaPlayer(videoView)
-//        controller.isEnabled = true
-//        controller.show()
-//        controller.setMediaPlayer(videoView)
-//        videoView.setMediaController(controller)
-//        videoView.setVideoPath(url)
-//        videoView.start()
-//    }
-//
-//    private fun clickListener(context: Context) {
-//        val supportFragmentManager = (context as AppCompatActivity).supportFragmentManager
-//        buyNowButton.setOnClickListener {
-//            ProductDetailBottomSheet().apply {
-//                show(supportFragmentManager, ProductDetailBottomSheet.TAG)
-//            }
-//        }
-//    }
-//
-//    @SuppressLint("SetTextI18n")
-//    fun bind(context: Context, product: ProductsQuery.Product) {
-//        title.text = product.title
-//        productPrice.text = "Rs. ${product.price}"
-//        vendor.text = product.vendor
-//        clickListener(context)
-//
-//        Picasso.with(context).load(product.photoUrl).into(imageView)
-//        playVideo(context, product.videoUrl)
-//    }
-//
-//}
